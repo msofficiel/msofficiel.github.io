72390cb512c4612a244b6c9ecdf3d01bb84a85e5
Oumar Abdelkerim MAHAMAT SALEH <msoumarabdelkerim@gmail.com>
2026-04-15 18:36:53 +0200

    Remove custom cursor tracking on mousemove
    Removed custom cursor functionality from mousemove event.

--- script.js
+++ script.js
@@ -72,9 +72,4 @@ document.addEventListener("DOMContentLoaded", () => {
   revealOnScroll(); // Lancer au chargement
 
 });
-const cursor = document.getElementById("cursor");
 
-document.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.clientX + "px";
-  cursor.style.top = e.clientY + "px";
-});
