b50ea09abeb5057bb1d81e7afbcf2501f1b968b7
Oumar Abdelkerim MAHAMAT SALEH <msoumarabdelkerim@gmail.com>
2026-04-15 18:31:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -72,3 +72,9 @@ document.addEventListener("DOMContentLoaded", () => {
   revealOnScroll(); // Lancer au chargement
 
 });
+const cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
